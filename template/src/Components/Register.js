299bf18f7fdb77f09d5210c0b2f03b24e71568e7
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-06-22 18:23:50 -0600

    skeleton Register component

---
+++ template/src/Components/Register.js
@@ -0,0 +1,34 @@
+import React, { useContext } from "react"
+import useInput from "../hooks/useInput"
+import { UserContext } from "../context/UserContext"
+
+const Register = () => {
+  const { register } = useContext(UserContext)
+  const [{ username, email, password }, setInput] = useInput({
+    username: "",
+    email: "",
+    password: "",
+  })
+  return (
+    <div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          register({ username, email, password })
+        }}
+      >
+        <input name="username" value={username} onChange={setInput} />
+        <input name="email" value={email} onChange={setInput} />
+        <input
+          name="password"
+          type="password"
+          value="password"
+          onChange={setInput}
+        />
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  )
+}
+
+export default Register
