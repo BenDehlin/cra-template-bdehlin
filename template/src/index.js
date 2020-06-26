b1ee0c2d83102b5658e8d26b1a60c7a6473750d5
Benjamin Dehlin <ben.dehlin@gmail.com>
2020-06-25 22:05:18 -0600

    bug fix and removed unneeded files

--- template/src/index.js
+++ template/src/index.js
@@ -2,32 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
-import reportWebVitals from "./reportWebVitals"
 import { HashRouter as Router } from "react-router-dom"
-import { Provider } from "react-redux"
-import store from "./redux/store"
 import { UserProvider } from "./context/UserContext"
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <Provider store={store}>
-        <UserProvider>
-          <App />
-        </UserProvider>
-      </Provider>
+      <UserProvider>
+        <App />
+      </UserProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
 )
-serviceWorker.unregister()
-reportWebVitals()
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
